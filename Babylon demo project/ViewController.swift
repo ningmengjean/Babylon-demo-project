a27bbb60e79f0277a9352d8d45af439148e1fedf
ningmengjean <ningmengjean@gmail.com>
2017-10-19 13:26:06 +0800

    Initial Commit

---
+++ Babylon demo project/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Babylon demo project
+//
+//  Created by wangchi on 2017/10/19.
+//  Copyright © 2017年 Zhu xiaojin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
